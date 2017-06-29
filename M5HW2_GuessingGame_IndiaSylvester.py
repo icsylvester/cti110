9733a3b8abcb945a77d0b811190f56d0a78733e5
icsylvester <sylvesti8561@student.faytechcc.edu>
2017-06-29 13:53:52 -0400

    Add files via upload

--- M5HW2_GuessingGame_IndiaSylvester.py
+++ M5HW2_GuessingGame_IndiaSylvester.py
@@ -1,5 +1,5 @@
 # A program that get grades and calculates the testing averages
-# 6/28/17
+# 6/29/17
 # CTI-110 M5HW2 - Random Number Guessing Game
 # India Sylvester
 
@@ -8,23 +8,75 @@ import random
 
 # Call the main function
 def main():
+
+    #generate a random number
+    answer = random.randint(1,5)
+
+    #Greet the user and get their input
+    guess = int(input("Hey! Let's play a game! I am thinking of" +
+                          " a number between 1 and 100. Can you" +
+                          "guess it? : "))
+        
+    #for loop to max out the number of guess
+    for i in range(1,6): 
+
+        #If the user guesses correctly, then terminate the program
+        correct = check_guess(guess, answer)
+        #If correct answer was stated
+        if (correct == True):
+              #give the user back the total number of guesses
+            print(" The total number of guesses were " + str(i))
+
+            #give user the previous answer
+            print("The answer was " + str(answer))
+           #Stop program
+            break
+        else:
+            int(input("Enter in another guess: "))
+        
+        #Ask user if they want to play again
+                
+    confirm = str(input("Do you want to play again? : "))
+
+        #If they say yes
+    if(confirm == "yes"):
+
+            #loop it back to input
+             int(input(" Enter in your guess: "))
+
+        #terminate the program if they say no
+    elif(confirm == "no"):
     
-    guess = int(input(" Enter your guess in the range of 0 and 100: "))
 
-    check_guess(guess)
+            #say a goodbye
+            print(" Enjoy your day! ")
 
-def check_guess(crguess):
-
-    answer = random.randrange(100)
-    
-    if(crguess == answer):
-        print("You guessed it")
+    #just incase they try to type anything other than the two confirmations
     else:
-        total = 0
-        for i in range(1, 100):
-            print("Try again")
-            tries = int(input("Enter in your guess: "))
+            print("Enter a yes or a no.")
+            confirm = str(input("Do you want to play again? : "))
             
-            total += tries
+#Define the checking answer
+def check_guess(crguess, answer):
 
+    #if the answer is correct
+    if(crguess == answer):
+
+       #coingratulate the user 
+        print("You guessed it!")
+        
+        return True
+
+    #if the user was wrong
+    else:
+        #if number guess was too high
+        if(crguess > answer):
+             print("The number was too high. ")
+
+        #if number guess was too low    
+        else:
+             print("The number was too low. ")
+        return False
+            
+#call the main            
 main()
